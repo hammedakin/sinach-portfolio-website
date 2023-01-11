84f6404b3fc679797528ec10b49fdcdb2fa6dddc
Hammed Akinyemi <hakra911@gmail.com>
2023-01-11 23:15:45 +0100

    Repute case study added

---
+++ src/components/CaseStudy/CaseStudy.styled.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const StyledRefarmB = styled.main`
+
+  h3 {
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 58px;
+    /* margin-bottom: 2rem; */
+
+    background: linear-gradient(180deg, #ffffff 0%, #408cff 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+  }
+
+ h4 {
+    font-size: 35px;
+    margin-bottom: 1rem;
+    color: #ffffff;
+  }
+
+ h5 {
+    font-size: 25px;
+    margin-bottom: 1rem;
+    color: #ffffff;
+  }
+ h6 {
+    font-size: 18px;
+    margin-bottom: 1rem;
+    color: #ffffff;
+  }
+  
+ p{
+   margin-bottom: 0.5rem;
+ }
+
+ p,
+ ul li,
+ ol li {
+   font-weight: 400;
+   font-size: 16px;
+   line-height: 29px;
+   color: #c4c4c4;
+
+ }
+
+ img {
+   margin-top:1.5rem;
+ }
+`;
+
+export const Section = styled.section`
+  margin-bottom: 3rem;
+
+`;
